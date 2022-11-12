a1f716cf0d658fa347fb051a2de5f7adb367ad82
James-McClure <s3898327@student.rmit.edu.au>
2022-11-12 17:35:39 +1100

    added dabase to gitignore

--- dao.py
+++ dao.py
@@ -5,34 +5,30 @@ class Dao:
     '''Data access object for lap data'''
 
     def __init__(self, filename) -> None:
-        self.filepath = "database/" + filename
+        self.filepath = "./database/" + filename
 
-    def save(self, json_string):
+    def save(self, json_data):
         '''Saves object to opened file'''
         print(self.filepath)
-        with open(self.filepath, "r+") as f:
+        with open(self.filepath, "r") as f:
             db_string = f.read()
             db_data = json.loads(db_string)
         
-    
+
             print(type(db_data))
             print(db_data)
-            db_data.append(json_string)
-
-
-
-        with open(self.filepath, "w") as f:
-            json.dump(db_data, f)
-            f.write("Test")
-            print("here")
-            f.close()
+            db_data.append(json_data)
+            print(db_data)
+            print(f.name)
+        
+        with open(self.filepath, "w+") as f:
+            json.dump(db_data, f, indent=4)
     
     def get_json(self):
         '''Returns file as a json string'''
         
         with open(self.filepath, "r") as f:
             db_string = json.loads(f)
-            f.close()
         
         return db_string
     
