7884f5da7849a743a50eb7d020eee6efca57ff07
James-McClure <s3898327@student.rmit.edu.au>
2022-11-16 07:59:24 +1100

    updated tests

--- run_tests.py
+++ run_tests.py
@@ -4,13 +4,14 @@ import main
 import dao
 
 if __name__ == "__main__":
-    '''socket_tests.run_lap_completed_test(
+    socket_tests.run_lap_completed_test(
         main.on_session_start,
         main.on_player_join, 
         main.on_car_info, 
         main.on_lap_completed,
-        main.on_player_leave
-        )'''
+        main.on_player_leave,
+        main.on_end_sesion
+        )
 
     dao = dao.Dao("laps.json")
-    dao_tests.load_dataframe_test(dao)
+    #dao_tests.load_dataframe_test(dao)
