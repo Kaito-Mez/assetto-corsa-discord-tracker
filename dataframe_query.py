755033a2ddec3045f8def689a53641d2b0432b96
James-McClure <s3898327@student.rmit.edu.au>
2022-11-16 17:06:36 +1100

    implemented the dataframe query module

---
+++ dataframe_query.py
@@ -0,0 +1,65 @@
+from models.lap_data import LapData
+from dao import Dao
+
+def get_player_fastest_lap(guid) -> LapData or None:
+    '''Gets the fasest lap of specified player'''
+
+    fastest_lap = None
+
+    dao = Dao("laps.json")
+    all_laps = dao.get_dataframe()
+    player_laps = all_laps[all_laps.guid == guid]
+    fastest_laps = player_laps[player_laps.laptime == player_laps.laptime.min()]
+
+    try:
+        fastest_lap_df = fastest_laps.iloc[0].to_dict()
+        fastest_lap = LapData(json_lap_data = fastest_lap_df)
+    except IndexError as e:
+        pass
+
+    return fastest_lap
+    
+
+def get_car_fastest_lap(car) -> LapData or None:
+    '''Gets the fastest lap of specified car'''
+
+    fastest_lap = None
+
+    dao = Dao("laps.json")
+    all_laps = dao.get_dataframe()
+    car_laps = all_laps[all_laps.car == car]
+    fastest_laps = car_laps[car_laps.laptime == car_laps.laptime.min()]
+
+    try:
+        fastest_lap_df = fastest_laps.iloc[0].to_dict()
+        fastest_lap = LapData(json_lap_data = fastest_lap_df)
+    except IndexError as e:
+        pass
+
+    return fastest_lap
+
+def get_player_fastest_lap_in_car(guid, car) -> LapData:
+    '''Gets a specific players fastest lap in a specific car'''
+    
+    fastest_lap = None
+    
+    dao = Dao("laps.json")
+    all_laps = dao.get_dataframe()
+    player_laps = all_laps[all_laps.guid == guid]
+    car_laps = player_laps[player_laps.car == car]
+    fastest_laps = car_laps[car_laps.laptime == car_laps.laptime.min()]
+
+    try:
+        fastest_lap_df = fastest_laps.iloc[0].to_dict()
+        fastest_lap = LapData(json_lap_data = fastest_lap_df)
+    except IndexError as e:
+        pass
+
+    return fastest_lap
+
+
+'''TESTS'''
+if __name__ == "__main__":
+    print(get_player_fastest_lap(76561198249901870))
+    print(get_car_fastest_lap("pschd_honda_integra_dc2_typer_1998"))
+    print(get_player_fastest_lap_in_car(76561198249901870, "ks_toyota_supra_mkiv_tuned"))
