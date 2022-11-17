76d3b3c1f5016c61fe38400d2761a784816f5f91
James-McClure <s3898327@student.rmit.edu.au>
2022-11-17 18:34:04 +1100

    added get_car_playtime

--- dataframe_query.py
+++ dataframe_query.py
@@ -39,7 +39,7 @@ def get_car_fastest_lap(car) -> LapData or None:
 
     return fastest_lap
 
-def get_player_fastest_lap_in_car(guid, car) -> LapData:
+def get_player_fastest_lap_in_car(guid, car) -> LapData or None:
     '''Gets a specific players fastest lap in a specific car'''
     
     fastest_lap = None
@@ -91,11 +91,42 @@ def get_all_players():
     
     return list(all_players)
 
+def get_player_playtime(guid):
+    '''Gets a player's total playtime on the server in seconds'''
+
+    dao = Dao("sessions.json")
+    all_sessions = dao.get_dataframe()
+    player_sessions = all_sessions[all_sessions.guid == guid]
+
+    total_playtime = player_sessions.session_time.sum()
+
+    return total_playtime
+
+def get_player_playtime_in_car(guid, car):
+    '''Gets a player's total playtime on the server in a specific car'''
+
+    dao = Dao("sessions.json")
+    all_sessions = dao.get_dataframe()
+    player_sessions = all_sessions[all_sessions.guid == guid]
+    car_sessions = player_sessions[player_sessions.car == car]
+
+    return car_sessions.session_time.sum()
+
+def get_car_playtime(car):
+    '''Gets the total playtime of a car regardless of player'''
+    dao = Dao("sessions.json")
+    all_sessions = dao.get_dataframe()
+    car_sessions = all_sessions[all_sessions.car == car]
+
+    return car_sessions.session_time.sum()
+
 '''TESTS'''
 if __name__ == "__main__":
     #print(get_player_fastest_lap(76561198249901871))
     #print(get_car_fastest_lap("pschd_honda_integra_dc2_typer_1998"))
-    print(get_player_fastest_lap_in_car(76561198249901870, "ks_toyota_supra_mkiv_tuned"))
+    print(get_player_fastest_lap_in_car(76561198249901870, "bksy_nissan_skyline_r34_z_tune"))
     #print(get_most_recent_lap())
     #print(get_average_laptime())
     #print(get_all_players())
+    print(get_player_playtime_in_car(76561198249901870, "bksy_nissan_skyline_r34_z_tune"))
+    print(get_car_playtime("bksy_nissan_skyline_r34_z_tune"))
